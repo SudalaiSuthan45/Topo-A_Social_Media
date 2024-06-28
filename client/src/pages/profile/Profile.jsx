4ba8911e895a86bc3baf4a5f2aaf5eb850892dde
SudalaiSuthan45 <sudalaisuthan3718@gmail.com>
2024-06-28 18:11:09 +0530

    Profile Home

--- client/src/pages/profile/Profile.jsx
+++ client/src/pages/profile/Profile.jsx
@@ -1,9 +1,32 @@
+import Feed from '../../components/feed/Feed'
+import Rightbar from '../../components/rightbar/Rightbar'
+import Sidebar from '../../components/sidebar/Sidebar'
+import Topbar from '../../components/topbar/Topbar'
 import './Profile.css'
 
 export default function Profile() {
   return (
-    <div>
-      
-    </div>
+    <>
+      <Topbar />
+      <div className="profile">
+        <Sidebar />
+        <div className="profileRight">
+            <div className="profileRightTop">
+                <div className="profileCover">
+                    <img src="assets/post/3.jpeg" alt="" className='profileCoverImg'/>
+                    <img src="assets/person/7.jpeg" alt="" className='profileUserImg'/>
+                </div>
+                <div className="profileInfo">
+                    <h4 className="profileInfoName">Safak</h4>
+                    <span className="profileInfoDesc">Helooo</span>
+                </div>
+            </div>
+            <div className="profileRightBottom">
+                <Feed />
+                <Rightbar profile/>
+            </div>
+        </div>
+      </div>
+    </>
   )
 }
